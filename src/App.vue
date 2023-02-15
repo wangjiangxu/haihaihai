3c2387ff44606ca75ad7057fa09ecfd9e422172b
hanhan <1933435684@qq.com>
2023-02-15 20:31:39 +0800

    抽取路由文件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-    <div>
-        <NButton>不可以</NButton>
-    </div>
-
+<!--  路由出口-->
+<!--路由匹配到的组件将渲染在这里-->
+    <router-view></router-view>
+    <router-link to="/">Go to Home</router-link>
+    |
+    <router-link to="/login">Go to Login</router-link>
 </template>
 
 <script setup lang="ts">
@@ -20,16 +14,5 @@
 </script>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import {createWebHistory,createRouter} from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+
+// 2.定义一些路由
+// 每个路由都需要映射到一个组件
+// 我们后面在讨论嵌套路由
+const routes = [
+    {path:'/',component:Home},
+    {path: '/login',component: Login}
+]
+
+// 3.创建路由并传递‘routes’配置
+// 这里可以输入更多的配置，但我们在这里暂时保持简单
+const router = createRouter({
+    // 4. 内部提供了 history模式的实现。为了简单期间我们这里使用 hash
+    history:createWebHistory(),
+    routes, // 'routes:routes' 的缩写
+})
+
+export default router
